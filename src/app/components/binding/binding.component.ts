dcbbfcc70e4fe708cdaa9902bdb616d96a8754fa
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-02-01 23:54:41 +0100

    add new functions

--- src/app/components/binding/binding.component.ts
+++ src/app/components/binding/binding.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-binding',
@@ -21,11 +21,34 @@ export class BindingComponent {
         Validators.required,
         Validators.minLength(8),
       ]),
-      agree: new FormControl(false, Validators.requiredTrue),
       name: new FormGroup({
         firstName: new FormControl(''),
         lastName: new FormControl(''),
       }),
+      agree: new FormControl(false, Validators.requiredTrue),
+      contacts: new FormArray([
+        new FormControl('+38(0', [
+          Validators.pattern(/\+38\(0[0-9]{2}\)-[0-9]{3}-\d{2}-\d{2}/),
+          Validators.required,
+        ]),
+      ]),
     });
+  }
+
+  onSubmit() {
+    this.loginForm.reset();
+  }
+
+  get getContacts(): FormArray {
+    return this.loginForm.get('contacts') as FormArray;
+  }
+
+  addPhone() {
+    this.getContacts.push(
+      new FormControl('+38(0', [
+        Validators.pattern(/\+38\(0[0-9]{2}\)-[0-9]{3}-\d{2}-\d{2}/),
+        Validators.required,
+      ])
+    );
   }
 }
